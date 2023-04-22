fc8de8ff782f27dbe2e9feb9d6e47e9f278b2b1a
Miniyahil Kebede <miniye6453@gmail.com>
2023-04-22 12:48:26 +0300

    introduced constants

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { Button, Card, CardBody, CardFooter, Checkbox, Input, Option, Select, Ty
 import {
   ArrowLongRightIcon,
   ArrowPathIcon,
- 
+
 } from "@heroicons/react/24/outline"
+import { AMHARIC_LETTERS, ENGLISH_LETTERS, NUMBERS, SYMBOLS } from './shared/constants';
 function App() {
   const generateText = () => {
     let result = '';
-    const amharic_letters = 'ሀሁሂሃሄህሆለሉሊላሌልሎሐሑሒሓሔሕሖመሙሚማሜምሞሠሡሢሣሤሥሦረሩሪራሬርሮሰሱሲሳሴስሶሸሹሺሻሼሽሾቀቁቂቃቄቅቆቇበቡቢባቤብቦቨቩቪቫቬቭቮተቱቲታቴትቶቸቹቺቻቼችቾነኑኒናኔንኖኘኙኚኛኜኝኞአኡኢኣኤእኦከኩኪካኬክኮኰኲኳኴኵኸኹኺኻኼኽኾወዉዊዋዌውዎዏዐዑዒዓዔዕዖዘዙዚዛዜዝዞዟዠዡዢዣዤዥዦየዩዪያዬይዮዯደዱዲዳዴድዶዷዸዹዺዻዼዽዾዿጀጁጂጃጄጅጆጇገጉጊጋጌግጎጏጐጒጓጔጕጠጡጢጣጤጥጦጧጨጩጪጫጬጭጮጯጰጱጲጳጴጵጶጷጸጹጺጻጼጽጾፀፁፂፃፄፅፆፇፈፉፊፋፌፍፎፐፑፒፓፔፕፖፗፘ';
-    const alphabet = includeEnglish ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : '';
-    const numbers = includeNumbers ? '0123456789' : '';
-    const symbols = includeSymbols ? '!@#$%^&*()+' : '';
-    let characters = amharic_letters + alphabet + numbers + symbols
+    const alphabet = includeEnglish ? ENGLISH_LETTERS : '';
+    const numbersToShow = includeNumbers ? NUMBERS : '';
+    const symbolsToShow = includeSymbols ? SYMBOLS : '';
+    let characters = AMHARIC_LETTERS + alphabet + numbersToShow + symbolsToShow;
     const charactersLength = characters.length;
     let counter = 0;
     while (counter < passwordLength) {
@@ -54,15 +54,15 @@ function App() {
             <Checkbox checked={includeEnglish} onChange={(e) => setIncludeEnglish(!includeEnglish)} id='english-checkbox' label="Include English Alphabet{A-Z}" ripple={true} />
             <Checkbox checked={includeNumbers} onChange={(e) => setIncludeNumbers(!includeNumbers)} id='numbers-checkbox' label="Allow Numbers (0-9)" ripple={true} />
             <Checkbox checked={includeSymbols} onChange={(e) => setIncludeSymbols(!includeSymbols)} id='symbols-checkbox' label="Allow Symbols (!@#$%^&*()+)" ripple={true} />
-            <Button  className=" flex items-center gap-3 " size="sm" onClick={() => generateText()}>አመንጭ/Generate
-            <ArrowPathIcon strokeWidth={2} className=" w-5" />
-    
+            <Button className=" flex items-center gap-3 " size="sm" onClick={() => generateText()}>አመንጭ/Generate
+              <ArrowPathIcon strokeWidth={2} className=" w-5" />
+
             </Button>
 
 
 
           </div>
-        
+
         </CardBody>
         <CardFooter>
           <Button className='flex items-center gap-3' variant="text"><a href='https://github.com/hmhard/amharic-password-generator'>View On Github</a> <ArrowLongRightIcon strokeWidth={2} className="h-5 w-5" /> </Button>
