3ddec022097becd141d451c450a239a07dc186f9
Miniyahil Kebede <miniye6453@gmail.com>
2023-04-23 12:12:57 +0300

    [feat]: added copy text to clipboard on clicking copy button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
-import { Button, Card, CardBody, CardFooter, Checkbox, Input, Option, Select, Typography } from '@material-tailwind/react';
+import {
+  Button, Card, CardBody, CardFooter, Checkbox, Input, Option, Select, Typography, Popover,
+  PopoverHandler,
+  PopoverContent,
+} from '@material-tailwind/react';
 import {
   ArrowLongRightIcon,
   ArrowPathIcon,
+  ClipboardIcon,
 
 } from "@heroicons/react/24/outline"
 import { AMHARIC_LETTERS, ENGLISH_LETTERS, NUMBERS, SYMBOLS } from './shared/constants';
+import copy from 'copy-to-clipboard';
+
+
 function App() {
+
+  const [passwordLength, setpasswordLength] = useState(12)
+  const [includeEnglish, setIncludeEnglish] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+  const [openPopover, setOpenPopover] = useState(false)
+  const [_,setCopied] = useState(false)
+  const [password, setPassword] = useState("")
+  const number_sequence = [...Array(30)].map((i, index) => index + 10);
   const generateText = () => {
     let result = '';
     const alphabet = includeEnglish ? ENGLISH_LETTERS : '';
@@ -20,23 +37,43 @@ function App() {
       counter += 1;
     }
     setPassword(result)
-  }
-  const [passwordLength, setpasswordLength] = useState(12)
-  const [includeEnglish, setIncludeEnglish] = useState(false)
-  const [includeNumbers, setIncludeNumbers] = useState(false)
-  const [includeSymbols, setIncludeSymbols] = useState(false)
-  const [password, setPassword] = useState("")
-  const number_sequence = [...Array(30)].map((i, index) => index + 10);
+  };
+
+  const handleCopy = () => {
+    setOpenPopover(true);
+    copy(password);
+    setCopied(true);
+    setTimeout(() => {
+      setOpenPopover(false);
+    }, 2000);
+  };
+
+
   return (
 
     <div>
-      <Card className="w-full ">
+      <Card className="w-full gap-4 max-w-[100rem] ">
 
         <CardBody className="flex flex-row gap-4 flex-col ">
 
           <Typography variant='h3' >የአማርኛ የይለፍ ቃል አመንጪ</Typography>
           <Typography variant='h3' > Amharic Password Generator</Typography>
-          <Input size="lg" value={password} readOnly />
+          <div className="relative flex w-full   mr-5">
+
+            <Input size="lg" value={password} readOnly />
+            <Popover open={openPopover} handler={handleCopy}>
+              <PopoverHandler >
+                <Button color="blue" onClick={handleCopy} >
+                  <ClipboardIcon strokeWidth={2} className=" w-5" />
+
+                </Button>
+              </PopoverHandler>
+              <PopoverContent>
+                Copied Successfully
+              </PopoverContent>
+            </Popover>
+
+          </div>
 
           <div className="flex flex-row gap-4 ">
             <div className="my-4 flex flex-row items-center gap-4 min-w-[12px]">
@@ -45,24 +82,17 @@ function App() {
               <Select variant="outlined" onChange={(e) => setpasswordLength(Number(e))} value={"" + passwordLength} label="Select Password Length">
                 {number_sequence.map((index) =>
                   <Option value={"" + index} key={index + 1}>{index}</Option>
-                )
-                }
-
+                )}
               </Select>
             </div>
 
             <Checkbox checked={includeEnglish} onChange={(e) => setIncludeEnglish(!includeEnglish)} id='english-checkbox' label="Include English Alphabet{A-Z}" ripple={true} />
             <Checkbox checked={includeNumbers} onChange={(e) => setIncludeNumbers(!includeNumbers)} id='numbers-checkbox' label="Allow Numbers (0-9)" ripple={true} />
             <Checkbox checked={includeSymbols} onChange={(e) => setIncludeSymbols(!includeSymbols)} id='symbols-checkbox' label="Allow Symbols (!@#$%^&*()+)" ripple={true} />
-            <Button className=" flex items-center gap-3 " size="sm" onClick={() => generateText()}>አመንጭ/Generate
+            <Button className=" flex items-center gap-3 p-1 px-3 " size="sm" onClick={() => generateText()}>አመንጭ/Generate
               <ArrowPathIcon strokeWidth={2} className=" w-5" />
-
             </Button>
-
-
-
           </div>
-
         </CardBody>
         <CardFooter>
           <Button className='flex items-center gap-3' variant="text"><a href='https://github.com/hmhard/amharic-password-generator'>View On Github</a> <ArrowLongRightIcon strokeWidth={2} className="h-5 w-5" /> </Button>
